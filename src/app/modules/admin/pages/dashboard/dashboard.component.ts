f1cfd1c19662695507c70f5e27213656f2cdfa20
María Paula <mapachass-16@hotmail.com>
2024-09-02 16:27:49 -0600

    dashboard cards

--- src/app/modules/admin/pages/dashboard/dashboard.component.ts
+++ src/app/modules/admin/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatInputModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
-
+  accounts: number = 50;
+  users: number = 30;
+  monitored: number = 20;
+  devices: number = 10;
 }
