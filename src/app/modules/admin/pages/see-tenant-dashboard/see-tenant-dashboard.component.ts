13193d9263848b96ed73cd772435df8803cd9513
María Paula <mapachass-16@hotmail.com>
2024-09-08 20:20:58 -0600

    feat: see users, senior citizens, devices IoT

--- src/app/modules/admin/pages/see-tenant-dashboard/see-tenant-dashboard.component.ts
+++ src/app/modules/admin/pages/see-tenant-dashboard/see-tenant-dashboard.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, AfterViewInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, AfterViewInit, ViewChildren, QueryList, ViewChild } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatPaginator, MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { Router } from '@angular/router'; // Corrige el import de Router
+import { Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,7 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { CommonModule } from '@angular/common';
 
-const Users = [
+const USERS = [
   {
     name: "Paula Chaves",
     email: "p.chaves@example.com",
@@ -39,31 +39,49 @@ const Users = [
   }
 ];
 
-const SENIORCITIZER = [
+const SENIORCITIZENS = [
   {
     name: "Alberto Campos",
-    email: "p.chaves@example.com",
-    role: "Owner"
+    gender: "Masculino",
+    age: "74"
   },
   {
-    name: "Carlos Mendoza",
-    email: "c.mendoza@example.com",
-    role: "User"
+    name: "Carmen Rojas",
+    gender: "Femenino",
+    age: "70"
   },
   {
-    name: "Laura Gómez",
-    email: "l.gomez@example.com",
-    role: "User"
+    name: "Luis Fonseca",
+    gender: "Masculino",
+    age: "67"
+  }
+];
+
+const DEVICESIOT = [
+  {
+    name: "Cuarto 1",
+    status: "On",
+    serialID: "744564651"
   },
   {
-    name: "Juan Pérez",
-    email: "j.perez@example.com",
-    role: "User"
+    name: "Sala",
+    status: "Off",
+    serialID: "456478415"
   },
   {
-    name: "Ana Martínez",
-    email: "a.martinez@example.com",
-    role: "User"
+    name: "Cocina",
+    status: "Suspended",
+    serialID: "759841235"
+  },
+  {
+    name: "Baño",
+    status: "On",
+    serialID: "984127836"
+  },
+  {
+    name: "Cuarto 2",
+    status: "On",
+    serialID: "671489657"
   }
 ];
 
@@ -79,17 +97,45 @@ export class SeeTenantDashboardComponent implements AfterViewInit {
   displayedUsersColumns: string[] = ['name', 'email', 'role', 'actions'];
   dataSourceUsers: MatTableDataSource<any>;
 
-  @ViewChild(MatPaginator) usersPaginator: MatPaginator;
-  @ViewChild(MatSort) usersSort: MatSort;
+  displayedSeniorCitizensColumns: string[] = ['name', 'gender', 'age', 'actions'];
+  dataSourceSeniorCitizens: MatTableDataSource<any>;
 
-  constructor(private _router: Router) {
-    this.dataSourceUsers = new MatTableDataSource(Users);
+  displayedDevicesIoTColumns: string[] = ['name', 'status', 'serialID', 'actions'];
+  dataSourceDevicesIoT: MatTableDataSource<any>;
+
+  @ViewChild('usersPaginator') usersPaginator: MatPaginator;
+  @ViewChild('usersSort') usersSort: MatSort;
+
+  @ViewChild('seniorCitizensPaginator') seniorCitizensPaginator: MatPaginator;
+  @ViewChild('seniorCitizensSort') seniorCitizensSort: MatSort;
+
+  @ViewChild('devicesIoTPaginator') devicesIoTPaginator: MatPaginator;
+  @ViewChild('devicesIoTSort') devicesIoTSort: MatSort;
+
+  constructor(
+    private _router: Router,
+    public _MatPaginatorIntl: MatPaginatorIntl,
+  ) {
+    this.dataSourceUsers = new MatTableDataSource(USERS);
+    this.dataSourceSeniorCitizens = new MatTableDataSource(SENIORCITIZENS);
+    this.dataSourceDevicesIoT = new MatTableDataSource(DEVICESIOT);
   }
 
   ngAfterViewInit() {
     this.dataSourceUsers.paginator = this.usersPaginator;
     this.dataSourceUsers.sort = this.usersSort;
+
+    this.dataSourceSeniorCitizens.paginator = this.seniorCitizensPaginator;
+    this.dataSourceSeniorCitizens.sort = this.seniorCitizensSort;
+
+    this.dataSourceDevicesIoT.paginator = this.devicesIoTPaginator;
+    this.dataSourceDevicesIoT.sort = this.devicesIoTSort;
+
+    this.usersPaginator._intl.itemsPerPageLabel = 'Datos por página';
+
+
   }
+
 
   applyFilterUsers(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -97,6 +143,24 @@ export class SeeTenantDashboardComponent implements AfterViewInit {
 
     if (this.dataSourceUsers.paginator) {
       this.dataSourceUsers.paginator.firstPage();
+    }
+  }
+
+  applyFilterSeniorCitizens(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceSeniorCitizens.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSourceSeniorCitizens.paginator) {
+      this.dataSourceSeniorCitizens.paginator.firstPage();
+    }
+  }
+
+  applyFilterDevicesIoT(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceDevicesIoT.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSourceDevicesIoT.paginator) {
+      this.dataSourceDevicesIoT.paginator.firstPage();
     }
   }
 }
