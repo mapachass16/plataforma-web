0919105ee53d7b941c58aef6a309253bb8fb2c87
María Paula <mapachass-16@hotmail.com>
2024-09-17 14:42:39 +0200

    feat: get users, monitored, IoT devices, medical devices

--- src/app/modules/admin/pages/see-tenant-dashboard/see-tenant-dashboard.component.ts
+++ src/app/modules/admin/pages/see-tenant-dashboard/see-tenant-dashboard.component.ts
@@ -1,61 +1,17 @@
-import { ChangeDetectionStrategy, Component, AfterViewInit, ViewChildren, QueryList, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, AfterViewInit, ViewChildren, QueryList, ViewChild, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatPaginator, MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { CommonModule } from '@angular/common';
+import { SupabaseService } from '../../../../services/supabase.service';
 
-const USERS = [
-  {
-    name: "Paula Chaves",
-    email: "p.chaves@example.com",
-    role: "Owner"
-  },
-  {
-    name: "Carlos Mendoza",
-    email: "c.mendoza@example.com",
-    role: "User"
-  },
-  {
-    name: "Laura Gómez",
-    email: "l.gomez@example.com",
-    role: "User"
-  },
-  {
-    name: "Juan Pérez",
-    email: "j.perez@example.com",
-    role: "User"
-  },
-  {
-    name: "Ana Martínez",
-    email: "a.martinez@example.com",
-    role: "User"
-  }
-];
-
-const SENIORCITIZENS = [
-  {
-    name: "Alberto Campos",
-    gender: "Masculino",
-    age: "74"
-  },
-  {
-    name: "Carmen Rojas",
-    gender: "Femenino",
-    age: "70"
-  },
-  {
-    name: "Luis Fonseca",
-    gender: "Masculino",
-    age: "67"
-  }
-];
 
 const DEVICESIOT = [
   {
@@ -107,6 +63,10 @@ const MEDICALDEVICES = [
 
 ];
 
+const OPTIONS: string[] = [
+  'On', 'Off'
+];
+
 @Component({
   selector: 'app-see-tenant-dashboard',
   standalone: true,
@@ -115,7 +75,13 @@ const MEDICALDEVICES = [
   styleUrls: ['./see-tenant-dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SeeTenantDashboardComponent implements AfterViewInit {
+export class SeeTenantDashboardComponent implements AfterViewInit, OnInit {
+  id: any;
+  user: any;
+  users: any = [];
+  monitored: any = [];
+  IoTDevices: any = [];
+
   displayedUsersColumns: string[] = ['name', 'email', 'role', 'actions'];
   dataSourceUsers: MatTableDataSource<any>;
 
@@ -142,27 +108,23 @@ export class SeeTenantDashboardComponent implements AfterViewInit {
 
   constructor(
     public _MatPaginatorIntl: MatPaginatorIntl,
+    private _route: ActivatedRoute,
+    private _supabaseService: SupabaseService,
   ) {
-    this.dataSourceUsers = new MatTableDataSource(USERS);
-    this.dataSourceSeniorCitizens = new MatTableDataSource(SENIORCITIZENS);
-    this.dataSourceDevicesIoT = new MatTableDataSource(DEVICESIOT);
     this.dataSourceMedicalDevices = new MatTableDataSource(MEDICALDEVICES);
   }
+  async ngOnInit() {
+    this.user = await this._supabaseService.getUserSession();
+    this.id = this._route.snapshot.paramMap.get('id');
+    await this.getMembers(this.id, this.user.data.user.role);
+    await this.getMonitored(this.id);
+    await this.getIoTDevices(this.id);
+  }
+
 
   ngAfterViewInit() {
-    this.dataSourceUsers.paginator = this.usersPaginator;
-    this.dataSourceUsers.sort = this.usersSort;
-
-    this.dataSourceSeniorCitizens.paginator = this.seniorCitizensPaginator;
-    this.dataSourceSeniorCitizens.sort = this.seniorCitizensSort;
-
-    this.dataSourceDevicesIoT.paginator = this.devicesIoTPaginator;
-    this.dataSourceDevicesIoT.sort = this.devicesIoTSort;
-
     this.dataSourceMedicalDevices.paginator = this.medicalDevicesPaginator;
     this.dataSourceMedicalDevices.sort = this.medicalDevicesSort;
-
-    this.usersPaginator._intl.itemsPerPageLabel = 'Datos por página';
   }
 
 
@@ -199,6 +161,98 @@ export class SeeTenantDashboardComponent implements AfterViewInit {
 
     if (this.dataSourceMedicalDevices.paginator) {
       this.dataSourceMedicalDevices.paginator.firstPage();
+    }
+  }
+
+  async getMembers(tenant_id: any, role: string) {
+    try {
+      if (role === "service_role") {
+        const { data, error } = await this._supabaseService.getTenantMembersService(tenant_id);
+        if (error) {
+          throw new Error(`Error al obtener los miembros del tenant (service_role): ${error.message}`);
+        }
+        for (let i = 0; i < data.length; i++) {
+          const member = data[i];
+          const newMember = {
+            name: member.firstname + ' ' + member.lastname,
+            email: member.email,
+            role: member.tenant_role,
+          };
+          this.users.push(newMember);
+        }
+      } else {
+        const { data, error } = await this._supabaseService.getTenantMembers(tenant_id);
+        if (error) {
+          throw new Error(`Error al obtener los miembros del tenant: ${error.message}`);
+        }
+        for (let i = 0; i < data.length; i++) {
+          const member = data[i];
+          const newMember = {
+            name: member.firstname + ' ' + member.lastname,
+            email: member.email,
+            role: member.tenant_role,
+          };
+          this.users.push(newMember);
+        }
+
+      }
+      this.dataSourceUsers = new MatTableDataSource(this.users);
+      this.dataSourceUsers.paginator = this.usersPaginator;
+      this.dataSourceUsers.sort = this.usersSort;
+      this.usersPaginator._intl.itemsPerPageLabel = 'Usuarios por página';
+
+    } catch (error: any) {
+      console.error('Error en getMembers:', error.message || error);
+    }
+  }
+
+  async getMonitored(tenant_id: any) {
+    try {
+      const { data, error } = await this._supabaseService.getMonitoredPeople(tenant_id);
+      if (error) {
+        throw new Error(`Error al obtener los monitoreados del tenant ${error.message}`);
+      }
+      for (let i = 0; i < data.length; i++) {
+        const monitored = data[i];
+        const newMonitored = {
+          name: monitored.first_name + ' ' + monitored.last_name,
+          gender: (monitored.gender == "M") ? "Masculino" : "Femenino",
+          age: monitored?.age ?? "No tiene dato",
+        };
+        this.monitored.push(newMonitored);
+      }
+      this.dataSourceSeniorCitizens = new MatTableDataSource(this.monitored);
+      this.dataSourceSeniorCitizens.paginator = this.seniorCitizensPaginator;
+      this.dataSourceSeniorCitizens.sort = this.seniorCitizensSort;
+      this.seniorCitizensPaginator._intl.itemsPerPageLabel = 'Monitoreados por página';
+
+    } catch (error: any) {
+      console.error('Error en getMonitored:', error.message || error);
+    }
+  }
+
+  async getIoTDevices(tenant_id: any) {
+    try {
+      const { data, error } = await this._supabaseService.getIoTDevicesByTenant(tenant_id);
+      if (error) {
+        throw new Error(`Error al obtener los dispositivos IoT del tenant ${error.message}`);
+      }
+      for (let i = 0; i < data.length; i++) {
+        const IoTDevice = data[i];
+        const newIoTDevice = {
+          name: IoTDevice.name,
+          status: OPTIONS[Math.floor(Math.random() * OPTIONS.length)],
+          serialID: IoTDevice.serial_number,
+        };
+        this.IoTDevices.push(newIoTDevice);
+      }
+      this.dataSourceDevicesIoT = new MatTableDataSource(this.IoTDevices);
+      this.dataSourceDevicesIoT.paginator = this.devicesIoTPaginator;
+      this.dataSourceDevicesIoT.sort = this.devicesIoTSort;
+      this.devicesIoTPaginator._intl.itemsPerPageLabel = 'Dispositivos IoT por página';
+
+    } catch (error: any) {
+      console.error('Error en getMonitored:', error.message || error);
     }
   }
 }
